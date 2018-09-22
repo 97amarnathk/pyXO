d6e8c87573c55f40ad3e07459e19f5d0b4a8dd95
akshitsoni <akshitsoni0000@gmail.com>
2018-09-22 17:59:22 +0530

    With MCTS agent

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from game import Game
 from board import Board
 from agent import Player, MiniMaxAgent, AlphaBetaMiniMaxAgent
+from MCTS import MCTSAgent
 
 p1 = MiniMaxAgent()
 p2 = AlphaBetaMiniMaxAgent()
+p3 = MCTSAgent()
+p4 = Player()
 
 board = Board(3)
-game = Game(board, p1, p2)
-game.start(verbose = True)
+game = Game(board, p1, p3)
+game.start(verbose = False)
